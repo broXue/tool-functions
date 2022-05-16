2cc545a28e5930c5bacdc2396df12c38caee999f
seed008 <1501970808@qq.com>
2022-05-16 14:12:42 +0800

    add-func

--- add.js
+++ add.js
@@ -1,9 +1,8 @@
-let add = (function (total) {
+let add1 = (function (total) {
   let allArgs = [];
   function _add(...args) {
     allArgs = [...allArgs, ...args];
     if(allArgs.length >= total){
-      console.log(222);
       let sum = allArgs.reduce((prev, cur) => prev + cur, 0);
       allArgs.length = 0; // 这里要把length卸载掉
       return sum;
@@ -13,6 +12,16 @@ let add = (function (total) {
   }
   return _add;
 })(5);
+
+function addFn(a, b, c, d, e) {
+  return a + b + c + d + e;
+}
+
+function curry(fn, ...args) {
+  return args.length < fn.length ? (...innerArgs) => curry(fn, ...args, ...innerArgs): fn(...args);
+}
+
+let add = curry(addFn);
 
 console.log(add(1,2,3,4,5));
 console.log(add(1)(2)(3)(4)(5));
